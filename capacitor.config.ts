275055ecfc63c12e2aa1636f2657823b7e894265
Marlon Harrison <marlon@ionic.io>
2024-09-10 10:58:46 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'geocoder-init',
+  webDir: 'www'
+};
+
+export default config;
